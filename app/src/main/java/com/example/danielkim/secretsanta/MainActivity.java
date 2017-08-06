3fc5e8fd59fea3e1ece201c504d2d32b30c95976
Daniel Kim <mck004@ucsd.edu>
2017-08-05 22:56:27 -0700

    Initial Activity Added

--- app/src/main/java/com/example/danielkim/secretsanta/MainActivity.java
+++ app/src/main/java/com/example/danielkim/secretsanta/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.danielkim.secretsanta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void OkBtnOnClick(View v) {
+        Button button = (Button) v;
+        ((Button) v).setText("Got it");
+
+    }
+
+
 }
