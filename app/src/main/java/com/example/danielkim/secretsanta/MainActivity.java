d0162564524b27f3f51e89548bcc4bc3df7bae48
Daniel Kim <mck004@ucsd.edu>
2017-08-18 19:09:04 -0700

    Second Activity w/ Passing data from first

--- app/src/main/java/com/example/danielkim/secretsanta/MainActivity.java
+++ app/src/main/java/com/example/danielkim/secretsanta/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.danielkim.secretsanta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText groupName_input, num_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OkBtnOnClick(View v) {
+        // Create button and intent for connecting activities
         Button button = (Button) v;
-        ((Button) v).setText("Got it");
+        Intent i = new Intent(MainActivity.this,name_nums.class);
 
+        // Grab the group name and number of people data
+        groupName_input = (EditText) findViewById(R.id.GNameInput);
+        num_input = (EditText) findViewById(R.id.numInput);
+
+        String GName = groupName_input.getText().toString();
+        String numPeople = num_input.getText().toString();
+        int num = Integer.parseInt(numPeople);
+
+        // Put data into the next activity
+        i.putExtra("GName",GName);
+        i.putExtra("numPeople", num);
+
+        // Start the next activity
+        startActivity(i);
     }
 
 
---
+++ app/src/main/java/com/example/danielkim/secretsanta/name_nums.java
@@ -0,0 +1,16 @@
+package com.example.danielkim.secretsanta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Daniel Kim on 8/18/2017.
+ */
+
+public class name_nums extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name_nums);
+    }
+}
