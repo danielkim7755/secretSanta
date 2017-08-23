c32f76479ec63df20703c12be59e26432470a709
Daniel Kim <mck004@ucsd.edu>
2017-08-23 15:18:37 -0700

    Added ScrollView and EditText Format Fix

--- app/src/main/java/com/example/danielkim/secretsanta/name_nums.java
+++ app/src/main/java/com/example/danielkim/secretsanta/name_nums.java
@@ -2,18 +2,18 @@ package com.example.danielkim.secretsanta;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Daniel Kim on 8/18/2017.
@@ -28,9 +28,24 @@ public class name_nums extends AppCompatActivity{
         // Back Button Setup
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // LinearLayout that holds edit texts.
         LinearLayout mainLayout = new LinearLayout(this);
         mainLayout.setOrientation(LinearLayout.VERTICAL);
 
+        // Scroll Layout that holds the main linear layout.
+        ScrollView scroll = new ScrollView(this);
+        scroll.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        scroll.addView(mainLayout);
+
+        //Instruction Edit Text
+        EditText instText = new EditText(this);
+        instText.setText("Enter names with their phone numbers", TextView.BufferType.NORMAL);
+        instText.setFocusable(false);
+        instText.setClickable(false);
+        instText.setTextColor(Color.RED);
+        instText.setBackgroundColor(Color.TRANSPARENT);
+        instText.setGravity(Gravity.CENTER_HORIZONTAL);
+        mainLayout.addView(instText);
 
         // Grab Data from first Activity
         Intent i = getIntent();
@@ -38,34 +53,36 @@ public class name_nums extends AppCompatActivity{
         int totalNum = i.getIntExtra("numPeople",0);
 
         // Dynamically set edit texts
-        List<String> NamesList = new ArrayList<String>();
-        List<String> NumList = new ArrayList<String>();
         String nameHint = "Name";
-        String numHint = "Number";
-
+        String numHint = "Phone Number";
 
         for (int j = 0; j < totalNum; j++)  {
             LinearLayout editTextLayout = new LinearLayout(this);
             editTextLayout.setOrientation(LinearLayout.HORIZONTAL);
             mainLayout.addView(editTextLayout);
 
+            // Name Edit Text
             EditText nameText = new EditText(this);
-
             nameText.setId(j);
             nameText.setHint(nameHint);
             nameText.setEllipsize(TextUtils.TruncateAt.MIDDLE);
-            nameText.setEms(30);
+            nameText.setEms(10);
             editTextLayout.addView(nameText);
 
+            // Number Edit Text
             EditText numText = new EditText(this);
             numText.setId(j);
             numText.setHint(numHint);
+            numText.setEms(7);
             editTextLayout.addView(numText);
-
         }
 
+        // Start Button
+        Button startButton = new Button(this);
+        
 
-        setContentView(mainLayout);
+
+        setContentView(scroll);
     }
 
 
