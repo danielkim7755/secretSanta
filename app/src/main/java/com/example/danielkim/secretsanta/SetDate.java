969d70f91194a2bff761c65ec05926c02e6e6f4d
Daniel Kim <mck004@ucsd.edu>
2017-08-24 14:34:41 -0700

    End and Restart Function Implementation

--- app/src/main/java/com/example/danielkim/secretsanta/SetDate.java
+++ app/src/main/java/com/example/danielkim/secretsanta/SetDate.java
@@ -3,8 +3,10 @@ package com.example.danielkim.secretsanta;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.text.SimpleDateFormat;
@@ -42,7 +44,27 @@ public class SetDate extends AppCompatActivity {
         DatePicker date = (DatePicker) findViewById(R.id.datePicker);
         String dueDate = getDate(date);
 
+        Button srtBtn = (Button) findViewById(R.id.startButton);
+        srtBtn.setText("Sending");
 
+        // TODO need to implement the random algorithm and txt messages
+
+
+
+
+        srtBtn.setText("Sent!");
+
+        // End and restart App
+        Handler handler = new Handler();
+        handler.postDelayed(
+            new Runnable()  {
+                public void run () {
+                    Intent intent = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            }
+        ,5000);
     }
 
     protected String getDate(DatePicker dtPck)  {
@@ -53,7 +75,7 @@ public class SetDate extends AppCompatActivity {
         Calendar cal = Calendar.getInstance();
         cal.set(year, month, day);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd yyyy");
         return dateFormat.format(cal.getTime());
     }
 }
